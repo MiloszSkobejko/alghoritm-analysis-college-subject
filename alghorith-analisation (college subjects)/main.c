5c022fe8b958b69f651ff7716e68aeec337c8da0
Miłosz Skobejko <miloszskobejko@icloud.com>
2023-03-19 14:29:13 +0100

    Initial Commit

---
+++ alghorith-analisation (college subjects)/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  alghorith-analisation (college subjects)
+//
+//  Created by Miłosz Skobejko on 19/03/2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
